33343979f7b1d009e88acebfc427ab6d6e22cf09
Fan Wang <ppdg@foxmail.com>
2015-07-30 13:52:46 +0000

    add song auto

--- hk.php
+++ hk.php
@@ -1,5 +1,5 @@
 <?php
-    system("git pull -f origin master");
-    system("git checkout -f master");
     system("git clean -f");
+    system("git checkout -f master");
+    system("git pull -f origin master");
 ?>
